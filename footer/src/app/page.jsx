11ea7ee64fed2c63a79e9efa867415110f2a1a74
gabriela-moleta <gabriela.moleta@aluno.senai.br>
2025-03-13 11:53:48 -0300

    ajuste

--- footer/src/app/page.jsx
+++ footer/src/app/page.jsx
@@ -6,7 +6,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Hello World</h1>
+      
       <Footer />
     </div>
   );
--- footer/src/components/footer/index.jsx
+++ footer/src/components/footer/index.jsx
@@ -1,5 +1,6 @@
 import style from "./footer.module.css";
 
+
 const Footer = () => {
   return (
     <footer className={style.footer}>
@@ -7,21 +8,16 @@ const Footer = () => {
         {/* Sobre */}
         <div className={style.about}>
           <h2 className={style.logo}>Clean.</h2>
-          <p>
-            O site Clean oferece produtos de skincare, maquiagem e cuidados com a pele...
+          <p className={style.description}>
+          O site Clean oferece produtos de skincare, maquiagem e cuidados com a pele, com um design minimalista e fácil de <br></br> navegar. Além disso, possui um blog com dicas de beleza, cuidados diários e resenhas de produtos. Ideal para quem busca <br></br>qualidade e informações sobre bem-estar.
+
           </p>
         </div>
+       
+    
 
-        {/* Links Relevantes */}
-        <div className={style.links}>
-          <h3>Relevantes</h3>
-          <ul>
-            <li><a href="#">Produtos de Skincare</a></li>
-            <li><a href="#">Produtos de Maquiagem</a></li>
-            <li><a href="#">Produtos de Corpo</a></li>
-            <li><a href="#">Blog</a></li>
-          </ul>
-        </div>
+
+       
 
         {/* Newsletter */}
         <div className={style.newsletter}>
@@ -35,9 +31,16 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className={style.bottom}>
-        <p>© 2025 Clean. Todos os direitos reservados.</p>
-      </div>
+     {/* Links Relevantes */}
+     <div className={style.links}>
+          <h3>Relevantes</h3>
+          <ul>
+            <li><a href="#">Produtos de Skincare</a></li>
+            <li><a href="#">Produtos de Maquiagem</a></li>
+            <li><a href="#">Produtos de Corpo</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </div>
     </footer>
   );
 }
